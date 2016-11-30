96d0100e471bce6a77f8f780f627989fd270112f
mateuszRybczonek <m.rybczonek@gmail.com>
2016-11-30 18:07:40 +0100

    using components to simplify templates

--- app/models/post.js
+++ app/models/post.js
@@ -1,9 +1,18 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+
+const { computed } = Ember;
 
 export default DS.Model.extend({
-  title: DS.attr('string'),
-  description: DS.attr('string'),
   category: DS.attr('string'),
+  description: DS.attr('string'),
+  title: DS.attr('string'),
+
   created_at: DS.attr('date', { defaultValue: new Date()}),
   updated_at: DS.attr('date', { defaultValue: new Date()}),
+
+  isTitleValid: computed.notEmpty('title'),
+  isDescriptionValid: computed.notEmpty('description'),
+  isCategoryValid: computed.notEmpty('category'),
+  isValid: computed.and('isTitleValid', 'isDescriptionValid', 'isCategoryValid')
 });
