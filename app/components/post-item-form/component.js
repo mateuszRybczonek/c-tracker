bbb285df5ae8a2df634c73fab383ca4796687256
mateuszRybczonek <m.rybczonek@gmail.com>
2016-12-09 11:17:18 +0100

    using ember-paper for new-post-form

--- app/components/post-item-form/component.js
+++ app/components/post-item-form/component.js
@@ -2,11 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  categories: ['Ruby on Rails', 'Ember', 'Other'],
+
   actions: {
 
     buttonClicked(param) {
       this.sendAction('action', param);
     }
   }
-
 });
--- app/models/post.js
+++ app/models/post.js
@@ -14,5 +14,6 @@ export default DS.Model.extend({
   isTitleValid: computed.notEmpty('title'),
   isDescriptionValid: computed.notEmpty('description'),
   isCategoryValid: computed.notEmpty('category'),
-  isValid: computed.and('isTitleValid', 'isDescriptionValid', 'isCategoryValid')
+  isValid: computed.and('isTitleValid', 'isDescriptionValid', 'isCategoryValid'),
+  isInvalid: computed.not('isValid'),
 });
