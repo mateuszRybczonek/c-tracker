c341be8d525493d476706ebfd63c46a7e925439e
mateuszRybczonek <m.rybczonek@gmail.com>
2016-12-19 18:12:37 +0100

    sorting posts

--- app/controllers/posts/index.js
+++ app/controllers/posts/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 const { Controller, computed } = Ember;
 
 export default Controller.extend({
-
-  isAuthenticated: computed.alias('session.isAuthenticated'),
+  sortProperties: ['created_at:desc'],
+  sortedPosts: computed.sort('model', 'sortProperties'),
 });
