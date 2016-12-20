cd61d06421ca6a498f395d4f3fdb476d676e3d50
mateuszRybczonek <m.rybczonek@gmail.com>
2016-12-20 17:02:01 +0100

    sorting options

--- app/controllers/posts/index.js
+++ app/controllers/posts/index.js
@@ -2,6 +2,25 @@ import Ember from 'ember';
 const { Controller, computed } = Ember;
 
 export default Controller.extend({
-  sortProperties: ['created_at:desc'],
+  sortBy: 'createdAtDesc',
+
+  sortProperties: Ember.computed('sortBy', function () {
+    let options = {
+      'createdAtAsc': 'created_at:asc',
+      'createdAtDesc': 'created_at:desc',
+    };
+    return options[this.get('sortBy')].split(',');
+  }),
+
   sortedPosts: computed.sort('model', 'sortProperties'),
+
+  stickyOptions: {
+  topSpacing: 65 //px, default: 0
+  },
+
+  actions: {
+    setSorting: function (option) {
+      this.set('sortBy', option);
+    },
+  },
 });
