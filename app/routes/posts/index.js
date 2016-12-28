1c3fcc2da8bdfb0fb20e3ccf88fa8ab2fc0e2fae
mateuszRybczonek <m.rybczonek@gmail.com>
2016-12-28 09:25:21 +0100

    removed artificial wait

--- app/routes/posts/index.js
+++ app/routes/posts/index.js
@@ -1,18 +1,9 @@
 import Ember from 'ember';
 const { Route } = Ember;
 
-import wait from 'library-app/utils/wait';
-
 export default Route.extend({
-  model: function() {
-    var posts = this.store.findAll('post');
-    return wait(posts, 3 * 1000);
+
+  model() {
+    return this.store.findAll('post');
   },
 });
-
-// export default Route.extend({
-//
-//   model() {
-//     return this.store.findAll('post');
-//   },
-// });
