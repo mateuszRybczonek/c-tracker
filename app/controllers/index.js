e5dbb5c8877906536fac9782f34a538729cfd129
mateuszRybczonek <m.rybczonek@gmail.com>
2017-01-19 11:39:24 +0100

    cleaning index controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,27 +1,6 @@
 import Ember from 'ember';
-const { Controller, computed } = Ember;
+const { Controller } = Ember;
 
 export default Controller.extend({
-
-  responseMessage: '',
-  emailAddress: '',
-
-  isValid: computed.match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: computed.not('isValid'),
-
-  actions: {
-
-    saveInvitation() {
-      const email = this.get('emailAddress');
-
-      const newInvitation = this.store.createRecord('invitation', {
-        email: email
-      });
-
-      newInvitation.save().then(() => {
-        this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-        this.set('emailAddress', '');
-      });
-    },
-  }
+  
 });
