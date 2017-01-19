b2c733ad7c50e7e693372344f2337c399d9e2b90
mateuszRybczonek <m.rybczonek@gmail.com>
2017-01-19 15:39:54 +0100

    refactor

--- app/routes/certificates/index.js
+++ app/routes/certificates/index.js
@@ -3,9 +3,11 @@ const { Route } = Ember;
 
 export default Route.extend({
   model() {
+    const currentUser = this.get('session.currentUser.email');
+
     return this.store.findAll('certificate')
       .then(results => results.filter((certificate) => {
-        return certificate.get('user') === this.get('session.currentUser.email');
+        return certificate.get('user') === currentUser;
       }));
   },
 });
