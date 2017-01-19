efd07ca3f3a731a5a0a99e9b26eb0b0c18feb452
mateuszRybczonek <m.rybczonek@gmail.com>
2017-01-19 12:28:09 +0100

    unit tests for controller certificates/index

--- tests/unit/controllers/certificates/index-test.js
+++ tests/unit/controllers/certificates/index-test.js
@@ -1,12 +1,58 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('controller:index', 'Unit | Controller | certificates | index', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+moduleFor('controller:certificates/index', 'Unit | Controller | certificates | index', {
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
+test('search by name', function(assert) {
   let controller = this.subject();
-  assert.ok(controller);
+
+  let certificate1 = Ember.Object.create({
+    name: 'Jedi master license',
+    number: 'GUM-123',
+    user: 'test@gmail.com',
+    type: 'STCW',
+    issueDate: new Date(2017, 1, 17),
+    expiryDate: new Date(2027, 1, 17),
+    updatedAt: new Date(2017, 1, 17),
+    comment: 'let the force be with you',
+  });
+
+  let certificate2 = Ember.Object.create({
+    name: 'Padawan (Jedi Apprentice) certificate',
+    number: 'GUM-345',
+    user: 'test@gmail.com',
+    type: 'STCW',
+    issueDate: new Date(2017, 1, 17),
+    expiryDate: new Date(2027, 1, 17),
+    updatedAt: new Date(2017, 1, 17),
+    comment: 'you can sense the force already',
+  });
+
+  let certificates = [ certificate1, certificate2 ];
+
+  controller.set('model', certificates);
+
+  const dataProvider = [
+    {
+      searchTerm: 'Non existing name',
+      result: [],
+      message: 'returns empty array for no match',
+    },
+    {
+      searchTerm: 'Jedi',
+      result: [ certificate1, certificate2],
+      message: 'returns multiple elements for multiple matches',
+    },
+    {
+      searchTerm: 'master',
+      result: [ certificate1],
+      message: 'returns single element for single match',
+    },
+  ];
+
+
+  dataProvider.forEach(testCase => {
+    controller.set('searchTerm', testCase.searchTerm);
+    assert.deepEqual(controller.get('matchingCertificates'), testCase.result, testCase.message)
+  });
 });
