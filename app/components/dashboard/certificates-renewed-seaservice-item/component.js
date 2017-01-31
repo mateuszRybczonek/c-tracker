1b4a34692361d84b3e57fc3e4c72c089925e9f59
mateuszRybczonek <m.rybczonek@gmail.com>
2017-01-31 11:56:56 +0100

    calculating daysOfSeaservice for certificates renewed based on seaservice

--- app/components/dashboard/certificates-renewed-seaservice-item/component.js
+++ app/components/dashboard/certificates-renewed-seaservice-item/component.js
@@ -1,9 +1,28 @@
 import Ember from 'ember';
+import { calculateDaysBetweenDates } from '../../../utils/date-utils';
 
-const { Component, computed, inject: { service } } = Ember;
+const { Component, computed } = Ember;
 
 export default Component.extend({
 
   tagName: 'tr',
   classNames: ['certificate'],
+
+  issueDate: computed.alias('certificate.issueDate'),
+
+  seaserviceSinceIssue: computed('certificate', 'seaservices', function() {
+    let seaserviceSinceIssue = [];
+    this.get('seaservices').map((seaservice) => {
+      if (new Date(this.get('issueDate')) <= new Date(seaservice.get('signOn'))) {
+        seaserviceSinceIssue.push(
+          calculateDaysBetweenDates(seaservice.get('signOff'), seaservice.get('signOn'))
+        );
+      }
+    });
+    return seaserviceSinceIssue.reduce((a, b) => a + b, 0);
+  }),
+  
+  missingSeaservice: computed('certificate', 'seaserviceSinceIssue', function() {
+    return this.get('certificate.daysOfServiceToRenew') - this.get('seaserviceSinceIssue');
+  }),
 });
