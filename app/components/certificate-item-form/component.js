38965818cd9698280b84ada74c509101b90fe6d7
mateuszRybczonek <m.rybczonek@gmail.com>
2017-03-10 13:56:59 +0100

    support for pdf and jpg

--- app/components/certificate-item-form/component.js
+++ app/components/certificate-item-form/component.js
@@ -28,16 +28,16 @@ export default Component.extend({
         $('.upload-successful').show(1000);
       };
       reader.readAsDataURL(files[0]);
-
       this.set('file', files[0]);
-
+      const file = this.get('file');
+      const fileExtension = file.name.split('.')[1];
       const metadata = {
-        contentType: 'image/png',
+        contentType: file.type,
       };
       const storageRef = this.get('firebaseApp').storage().ref();
-      const path = 'images/certificates/' + certificate.get('id') + '.png';
+      const path = 'images/certificates/' + certificate.get('id') + '.' + fileExtension;
 
-      const uploadTask = storageRef.child(path).put(this.get('file'), metadata);
+      const uploadTask = storageRef.child(path).put(file, metadata);
 
       uploadTask.on('state_changed', function(snapshot) {
         let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
@@ -46,37 +46,6 @@ export default Component.extend({
         const downloadURL = uploadTask.snapshot.downloadURL;
         certificate.set('imageUrl', downloadURL);
       });
-
-      // const storageRef = this.get('firebaseApp').storage().ref();
-      // let file = data;
-      // let uploadTask = storageRef.child('images/' + file[0].name).put(file[0]);
-      // uploadTask.on(this.get('firebaseApp').storage.TaskEvent.STATE_CHANGED,
-      //   (snapshot) => {
-      //     var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      //     console.log('Upload is ' + progress + '% done');
-      //     this.set('progressText', `Upload is ${Math.round(progress * 100) / 100} % done`);
-      //     this.set('progress', progress);
-      //     switch (snapshot.state) {
-      //       case this.get('firebaseApp').storage.TaskState.PAUSED:
-      //         this.set('status', 'Upload is paused');
-      //         break;
-      //       case this.get('firebaseApp').storage.TaskState.RUNNING:
-      //         this.set('status', 'Upload is running');
-      //         break;
-      //     }
-      //   }, (error) => {
-      //     switch (error.code) {
-      //       case 'storage/unauthorized':
-      //         break;
-      //       case 'storage/canceled':
-      //         break;
-      //       case 'storage/unknown':
-      //         break;
-      //     }
-      //   }, () => {
-      //     this.set('downloadURL', uploadTask.snapshot.downloadURL);
-      //   }
-      // );
     }
   },
 });
