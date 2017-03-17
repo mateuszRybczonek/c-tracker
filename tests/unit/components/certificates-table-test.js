5b579684a0173a04a50cc6664f594fc03867dea9
mateuszRybczonek <m.rybczonek@gmail.com>
2017-03-17 14:52:40 +0100

    seaservice-table unit test

--- tests/unit/components/certificates-table-test.js
+++ tests/unit/components/certificates-table-test.js
@@ -1,6 +1,4 @@
-import Ember from 'ember';
 import { test, moduleForComponent } from 'ember-qunit';
-import { createFutureDate } from 'library-app/utils/date-utils';
 
 moduleForComponent('certificates-table', 'Unit | Component | certificates-table',
   {
@@ -18,11 +16,11 @@ test('sortedCertificates', function(assert) {
 
   component.set('certificates', [third, fourth, second, first]);
   assert.deepEqual(component.get('sortedCertificates'), [first, second, third, fourth],
-    'sorts certificates in descending order based on expiryDate'
+    'sorts certificates in ascending order based on expiryDate'
   );
 });
 
-function _createExpiringCertificatesModelStub({ expiryDate }) {
+function _createExpiringCertificatesModelStub(expiryDate) {
   return {
     expiryDate: expiryDate,
   };
---
+++ tests/unit/components/seaservice-table-test.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+import { test, moduleForComponent } from 'ember-qunit';
+
+moduleForComponent('seaservice-table', 'Unit | Component | seaservice-table',
+  {
+    unit: true,
+  }
+);
+
+test('sortedSeaservice', function(assert) {
+  const component = this.subject();
+
+  const first = _createSeaserviceModelStub(new Date(2016, 3, 5) );
+  const second = _createSeaserviceModelStub(new Date(2016, 2, 5));
+  const third = _createSeaserviceModelStub(new Date(2015, 3, 5));
+  const fourth = _createSeaserviceModelStub(new Date(2015, 3, 4));
+
+  component.set('seaservice', [third, fourth, second, first]);
+  assert.deepEqual(component.get('sortedSeaservice'), [first, second, third, fourth],
+    'sorts seaservice in descending order based on signOff'
+  );
+});
+
+function _createSeaserviceModelStub(signOff) {
+  return {
+    signOff: signOff,
+  };
+}
