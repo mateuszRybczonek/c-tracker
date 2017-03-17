3f97339ed5fde019bafa0725b388d3fa456e05da
mateuszRybczonek <m.rybczonek@gmail.com>
2017-03-17 09:09:38 +0100

    redirecting when user logged in on login route

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 const { Route } = Ember;
 
 export default Route.extend({
-  beforeModel: function(){
+  beforeModel() {
     if(this.get('session.isAuthenticated')) {
       this.transitionTo('users.dashboard');
     } else {
--- tests/unit/routes/login-test.js
+++
@@ -1,9 +0,0 @@
-import { moduleFor, test } from 'ember-qunit';
-
-moduleFor('route:login', 'Unit | Route | login', {
-});
-
-test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
-});
