49542ea7749d8e21998e5038860bba27f8e34b9d
mateuszRybczonek <m.rybczonek@gmail.com>
2017-04-13 08:48:22 +0200

    certificates tables refactored

--- app/controllers/users/certificates/index.js
+++ app/controllers/users/certificates/index.js
@@ -29,4 +29,35 @@ export default Controller.extend({
   medicalCertificates: computed.filterBy('matchingCertificates', 'type', 'Medical'),
   endorsements: computed.filterBy('matchingCertificates', 'type', 'Endorsement'),
   other: computed.filterBy('matchingCertificates', 'type', 'Other'),
+
+  certificatesTables: computed('stcwCertificates',
+    'passports',
+    'medicalCertificates',
+    'endorsements',
+    'other', function() {
+      return [
+        {
+          certificates: this.get('stcwCertificates'),
+          title: 'STCW certificates',
+          id: 'stcwCerts',
+        }, {
+          certificates: this.get('passports'),
+          title: "Passports and Seaman's books",
+          id: 'passports',
+        }, {
+          certificates: this.get('medicalCertificates'),
+          title: 'Medical certificates',
+          id: 'medicalCerts',
+        }, {
+          certificates: this.get('endorsements'),
+          title: 'Endorsements',
+          id: 'endorsements',
+        }, {
+          certificates: this.get('other'),
+          title: 'Other',
+          id: 'other',
+        },
+      ];
+    }
+  ),
 });
