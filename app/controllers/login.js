92b9013ff7a834a423baa4e37840a04328496c4f
mateuszRybczonek <m.rybczonek@gmail.com>
2017-04-13 09:19:30 +0200

    exploreItem computed removed

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,32 +2,30 @@ import Ember from 'ember';
 const { Controller, computed } = Ember;
 
 export default Controller.extend({
-  exploreItems: computed(() => {
-    return [
-      {
-        icon: 'backup',
-        title: 'Access',
-        descriptionItems: [
-          'Have access to your data anywhere you are.',
-          'Use your mobile or desktop to access the data.'
-        ],
-      }, {
-        icon: 'assignment',
-        title: 'Control',
-        descriptionItems: [
-          'Keep track of your certificates and seaservice in simple and clear way.',
-          "Get information about expiring certificates before it's too late.",
-          'Know how many days more you need to renew your license based on seaservice.',
-        ],
-      }, {
-        icon: 'assessment',
-        title: 'Statistics',
-        descriptionItems: [
-          'Know how many days you spent onboard in the last year.',
-          'Let the application calculate your DP hours, day on board, etc.',
-          'Compare the amount of time spent onboard.',
-        ],
-      },
-    ];
-  }),
+  exploreItems: [
+    {
+      icon: 'backup',
+      title: 'Access',
+      descriptionItems: [
+        'Have access to your data anywhere you are.',
+        'Use your mobile or desktop to access the data.'
+      ],
+    }, {
+      icon: 'assignment',
+      title: 'Control',
+      descriptionItems: [
+        'Keep track of your certificates and seaservice in simple and clear way.',
+        "Get information about expiring certificates before it's too late.",
+        'Know how many days more you need to renew your license based on seaservice.',
+      ],
+    }, {
+      icon: 'assessment',
+      title: 'Statistics',
+      descriptionItems: [
+        'Know how many days you spent onboard in the last year.',
+        'Let the application calculate your DP hours, day on board, etc.',
+        'Compare the amount of time spent onboard.',
+      ],
+    },
+  ],
 });
