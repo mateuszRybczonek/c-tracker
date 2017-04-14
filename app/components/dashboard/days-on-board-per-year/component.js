3ff7be97c3bde44e94edf2e8278456f5d327cce4
mateuszRybczonek <m.rybczonek@gmail.com>
2017-04-14 09:29:39 +0200

    days on board chart mvp

---
+++ app/components/dashboard/days-on-board-per-year/component.js
@@ -0,0 +1,143 @@
+import Ember from 'ember';
+import colorPalette from 'library-app/consts/color-palette';
+import { calculateDaysBetweenDates } from 'library-app/utils/date-utils';
+
+const { DARK_GREY } = colorPalette;
+const { Component, computed, observer, inject: { service } } = Ember;
+
+export default Component.extend({
+  lazyLoader: service(),
+
+  seaserviceDaysPerYear: computed('seaservices', function() {
+    let seaserviceStats = [];
+    let thisYear = new Date().getFullYear();
+
+    this._createSeaserviceStatsForYear(thisYear-5, seaserviceStats);
+    this._createSeaserviceStatsForYear(thisYear-4, seaserviceStats);
+    this._createSeaserviceStatsForYear(thisYear-3, seaserviceStats);
+    this._createSeaserviceStatsForYear(thisYear-2, seaserviceStats);
+    this._createSeaserviceStatsForYear(thisYear-1, seaserviceStats);
+    this._createSeaserviceStatsForYear(thisYear, seaserviceStats);
+    return seaserviceStats;
+  }),
+
+  _createSeaserviceStatsForYear(year, seaserviceStats) {
+    let seaserviceGivenYear = [];
+    this.get('seaservices').map((seaservice) => {
+      const signOn = new Date(seaservice.get('signOn'));
+      const signOff = new Date(seaservice.get('signOff'));
+      const firstDayOfTheYear = new Date(year,0,1);
+      const lastDayOfTheYear = new Date(year,11,31);
+
+      if ((signOn.getFullYear() === year) && (signOff.getFullYear() === year)) {
+        seaserviceGivenYear.push(
+          calculateDaysBetweenDates(signOff, signOn)
+        );
+      } else if ((signOn.getFullYear() !== year) && (signOff.getFullYear() === year)) {
+        seaserviceGivenYear.push(
+          calculateDaysBetweenDates(signOff, firstDayOfTheYear)
+        );
+      } else if ((signOn.getFullYear() === year) && (signOff.getFullYear() !== year)) {
+        seaserviceGivenYear.push(
+          calculateDaysBetweenDates(lastDayOfTheYear, signOn)
+        );
+      }
+    });
+    let result = Math.ceil(seaserviceGivenYear.reduce((a, b) => a + b, 0));
+    seaserviceStats.push([year, result]);
+    console.log(seaserviceStats);
+  },
+
+  enoughStats: computed('seaserviceDaysPerYear', function() {
+    return this.get('seaserviceDaysPerYear').length > 2;
+  }),
+
+  graphLoading: computed('graph.{loaded,error}', function() {
+    return !(this.get('graph.loaded') || this.get('graph.error'));
+  }),
+
+  showGraph: computed('graph.error', 'graphLoading', function() {
+    return !(this.get('graph.error') || this.get('graphLoading'));
+  }),
+
+  drawGraph: observer('seaserviceDaysPerYear', function() {
+    this.drawChart();
+  }),
+
+  init() {
+    this._super(...arguments);
+    this.setProperties({
+      seaserviceDaysPerYear: this.get('seaserviceDaysPerYear') || [],
+      graph: this.get('graph') || {
+        loaded: false,
+        error: false,
+      },
+    });
+  },
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.prepareChart();
+  },
+
+  chartElementPresent() {
+    return this.$('#yearly-days-chart').length > 0;
+  },
+
+  drawChart() {
+    const seaserviceDaysPerYear = this.get('seaserviceDaysPerYear');
+
+    if (!this.chartElementPresent()) {
+      return;
+    }
+
+    window.nv.addGraph(function() {
+      const chart = window.nv.models.discreteBarChart()
+        .x((d) => d[0])
+        .y((d) => d[1])
+        .showValues(true)
+        .showYAxis(false)
+        .color([DARK_GREY])
+        .margin({ left: 0, top: 20, bottom: 30, right: 0 });
+
+      chart.tooltip.enabled(false);
+      chart.duration(2000);
+      chart.xAxis.tickPadding(18).tickFormat(d3.format('d'));
+      chart.yAxis.axisLabel('Days')
+        .tickFormat((d) => `${d3.format(',f')(d)} days`);
+
+      chart.valueFormat((d) => `${d3.format()(d)} days`);
+      window.d3.select('#yearly-days-chart svg').datum([{
+        values: seaserviceDaysPerYear,
+        key: 'Days',
+      }]).call(chart);
+
+      window.nv.utils.windowResize(chart.update);
+
+      return chart;
+    });
+  },
+
+  onGraphLibrariesLoaded() {
+    this.set('graph.loaded', true);
+    this.drawChart();
+  },
+
+  onGraphLibrariesLoadError() {
+    this.set('graph.error', true);
+    this.set('graph.loaded', false);
+  },
+
+  prepareChart() {
+    this._loadDependencies().then(
+      this.onGraphLibrariesLoaded.bind(this),
+      this.onGraphLibrariesLoadError.bind(this)
+    );
+  },
+
+  _loadDependencies() {
+    return this.get('lazyLoader').loadD3().then(() => {
+      return this.get('lazyLoader').loadNv();;
+    });
+  },
+});
---
+++ app/consts/color-palette.js
@@ -0,0 +1,3 @@
+export default {
+  DARK_GREY: '#222',
+};
