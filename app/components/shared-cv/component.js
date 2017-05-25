a18cb279c3e8912621fec8375b6f784cb0d11e5b
mateuszRybczonek <m.rybczonek@gmail.com>
2017-05-25 13:29:01 +0200

    using certificatesTables in shared-cv

--- app/components/shared-cv/component.js
+++ app/components/shared-cv/component.js
@@ -1,5 +1,43 @@
 import Ember from 'ember';
 
-const { Component } = Ember;
+const { Component, computed } = Ember;
 
-export default Ember.Component.extend({});
+export default Component.extend({
+  stcwCertificates: computed.filterBy('certificates', 'type', 'STCW'),
+  passports: computed.filterBy('certificates', 'type', 'Passport'),
+  medicalCertificates: computed.filterBy('certificates', 'type', 'Medical'),
+  endorsements: computed.filterBy('certificates', 'type', 'Endorsement'),
+  other: computed.filterBy('certificates', 'type', 'Other'),
+
+  certificatesTables: computed('stcwCertificates',
+    'passports',
+    'medicalCertificates',
+    'endorsements',
+    'other', function() {
+      return [
+        {
+          certificates: this.get('stcwCertificates'),
+          title: 'STCW certificates',
+          id: 'stcwCerts',
+          class: '',
+        }, {
+          certificates: this.get('passports'),
+          title: "Passports and Seaman's books",
+          id: 'passports',
+        }, {
+          certificates: this.get('medicalCertificates'),
+          title: 'Medical certificates',
+          id: 'medicalCerts',
+        }, {
+          certificates: this.get('endorsements'),
+          title: 'Endorsements',
+          id: 'endorsements',
+        }, {
+          certificates: this.get('other'),
+          title: 'Other',
+          id: 'other',
+        },
+      ];
+    }
+  ),
+});
